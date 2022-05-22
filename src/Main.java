69bb055d9acf13655bfd29d72ecd6c98af120d04
Anna Litvinova <litvinova4815@gmail.com>
2022-05-22 23:44:35 +0300

    Created Repository

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 18800; // цена билета
+        int mile = 20; // кол-во рублей за 1 милю
+        int bonus = price / mile; //кол-во бонусных миль
+        System.out.println("Вам начислено " + (bonus) + " бонусных миль");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
+ bonus = price / 20; // кол-во бонусных миль
+        ")*/
